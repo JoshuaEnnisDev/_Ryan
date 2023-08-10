74a5022b48f92aa928b0355e3665d5b0b87bc88c
JoshuaEnnisDev <joshuaennisdev@gmail.com>
2023-08-10 17:41:27 -0500

    commit

--- Pygame Zero/Runner/runner.py
+++ Pygame Zero/Runner/runner.py
@@ -1,8 +1,6 @@
 from pgzrun import go
 
-# animate snail
-# add score
-# collision
+
 # add fly
 
 WIDTH = 800
@@ -22,11 +20,14 @@ snail.bottom = 305
 snail_timer = 30
 
 game_started = False
+game_over = False
+score = 0
+
+BOX = Rect((300, 200), (200, 60))
 
 
 def draw():
-
-    if not game_started:
+    if not game_started and not game_over:
         screen.fill("blue")
         screen.blit('player/player_stand', (375, 200))
         screen.draw.text(
@@ -36,11 +37,21 @@ def draw():
             fontname='pixeltype',
             color=(255, 255, 255)
         )
+    elif game_over:
+        screen.fill('black')
+        screen.draw.filled_rect(BOX, 'red')
+        screen.draw.text(
+            "Game Over",
+            (325, 220),
+            fontsize=50,
+            fontname='pixeltype',
+            color='#FFFF00'
+        )
     else:
         screen.blit('sky', (0, 0))
         screen.blit('ground', (0, 300))
         screen.draw.text(
-            "My game",
+            f"Score: {score}",
             (350, 40),
             fontsize=50,
             fontname='pixeltype',
@@ -52,32 +63,46 @@ def draw():
 
 def on_key_down():
     global gravity
-    if keyboard.space and player.bottom >= 305:
+    if game_started and keyboard.space and player.bottom >= 305:
         gravity = -20
+        player.image = 'player/jump'
+        
 
 
 def update():
     global game_started
+    global game_over
     global gravity
     global snail_timer
+    global score
+
+    if snail.colliderect(player):
+        game_over = True
 
     # stuff that hapens when game is running
     if game_started:
         # player logic
-        snail_timer -= 1
-        # print(snail_timer)
+        player_timer -= 1
+        if player_timer <= 0:
+            
         gravity += 1
         player.y += gravity
         if player.bottom >= 305:
             player.bottom = 305
 
         # snail logic
+        snail_timer -= 1
         snail.x -= 4
         if snail_timer <= 0:
             snail_timer = 30
+            if snail.image == 'snail/snail1':
+                snail.image = 'snail/snail2'
+            else:
+                snail.image = 'snail/snail1'
 
         if snail.right <= 0:
             snail.left = WIDTH
+            score += 1
 
     if not game_started and keyboard.space:
         game_started = True
