ecbe8388f661f2f1e4152549c0e6a80b4ad2ca2d
JoshuaEnnisDev <joshuaennisdev@gmail.com>
2023-08-14 11:21:39 -0500

    update runner.py

--- Pygame Zero/Runner/runner.py
+++ Pygame Zero/Runner/runner.py
@@ -6,6 +6,7 @@ from pgzrun import go
 WIDTH = 800
 HEIGHT = 400
 TITLE = 'Runner'
+MAX_SPEED = 20
 
 # player actor
 player = Actor('player/player_walk_1')
@@ -18,12 +19,13 @@ snail = Actor('snail/snail1')
 snail.left = 600
 snail.bottom = 305
 snail_timer = 30
+snail_speed = 4
 
 game_started = False
 game_over = False
 score = 0
 
-BOX = Rect((300, 200), (200, 60))
+BOX = Rect((300, 200), (250, 160))
 
 
 def draw():
@@ -47,6 +49,13 @@ def draw():
             fontname='pixeltype',
             color='#FFFF00'
         )
+        screen.draw.text(
+            f"Final Score:{score}",
+            (325, 280),
+            fontsize=50,
+            fontname='pixeltype',
+            color='#FFFF00'
+        )
     else:
         screen.blit('sky', (0, 0))
         screen.blit('ground', (0, 300))
@@ -66,7 +75,6 @@ def on_key_down():
     if game_started and keyboard.space and player.bottom >= 305:
         gravity = -20
         player.image = 'player/jump'
-        
 
 
 def update():
@@ -74,25 +82,25 @@ def update():
     global game_over
     global gravity
     global snail_timer
+    global snail_speed
     global score
 
     if snail.colliderect(player):
         game_over = True
 
     # stuff that hapens when game is running
-    if game_started:
+    if game_started and not game_over:
         # player logic
-        player_timer -= 1
-        if player_timer <= 0:
-        
         gravity += 1
         player.y += gravity
         if player.bottom >= 305:
             player.bottom = 305
 
+        snail_speed = min(5 + score / 12, MAX_SPEED)
+
         # snail logic
         snail_timer -= 1
-        snail.x -= 4
+        snail.x -= snail_speed
         if snail_timer <= 0:
             snail_timer = 30
             if snail.image == 'snail/snail1':
